61b1914e99ab007a24970ead5a97ccc8a96bfd9d
Margal <mgalloperalta@gmail.com>
2019-04-16 21:58:57 +0200

    Fix date visualization

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -49,13 +49,17 @@ class Offers extends Component {
     return (
       <div>
         {this.state.offers.map((offer) => {
+          const fromISO = new Date(offer.from);
+          const from = fromISO.getFullYear()+'-' + (fromISO.getMonth()+1) + '-'+fromISO.getDate();
+          const untilISO = new Date(offer.until);
+          const until = untilISO.getFullYear()+'-' + (untilISO.getMonth()+1) + '-'+untilISO.getDate();
           return(
             
             <div key={offer._id}>
             <Link to={`/Offer/${offer._id}`}>
               <p>{offer.location}</p> 
-              <p>{offer.from}</p> 
-              <p>{offer.until}</p> 
+              <p>{from}</p> 
+              <p>{until}</p> 
               <p>{offer.budget}</p>
               </Link>
               <button onClick={()=>this.deleteOffer(offer._id)}>Delete</button> 
