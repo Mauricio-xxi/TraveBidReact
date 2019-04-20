225d6bdf65718e7b8b5d71affdb1d5bc49fbcb93
Álvaro Camacho <mauricio-xxi@hotmail.com>
2019-04-20 13:44:00 +0200

    add config in index.js to apollo client and provid

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient ({
+  networkInterface : createNetworkInterface({
+    uri: '/api',
+  }),
+})
 
 ReactDOM.render(
   <Router>
-    <App />
+    <ApolloProvider client ={client}>
+      <App />
+    </ApolloProvider>
   </Router>
 , document.getElementById('root'));
