d232213c0b350d6a8be9f7df51eb68c899124395
Margal <mgalloperalta@gmail.com>
2019-04-28 12:28:38 +0200

    Create login component rendered by login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,43 +1,13 @@
 import React, { Component } from "react";
 import { withAuth } from "../lib/AuthProvider";
+import UserLogin from "../components/user/UserLogin";
 
 class Login extends Component {
-  state = {
-    username: "",
-    password: ""
-  };
-
-  handleFormSubmit = event => {
-    event.preventDefault();
-    const { username, password } = this.state;
-    this.props.login({ username, password });
-  };
-
-  handleChange = event => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
 
   render() {
-    const { username, password } = this.state;
+
     return (
-      <form onSubmit={this.handleFormSubmit}>
-        <label>Username:</label>
-        <input
-          type="text"
-          name="username"
-          value={username}
-          onChange={this.handleChange}
-        />
-        <label>Password:</label>
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={this.handleChange}
-        />
-        <input type="submit" value="Login" />
-      </form>
+      <UserLogin />
     );
   }
 }
