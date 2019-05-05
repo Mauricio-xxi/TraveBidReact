355ce4d63b423c008232348296c7aba8f701a368
Margal <mgalloperalta@gmail.com>
2019-05-05 11:26:12 +0200

    Add bid decline & accept services + error catching

--- src/lib/bid-service.js
+++ src/lib/bid-service.js
@@ -14,22 +14,26 @@ class Bid {
       .then(({ data }) => {
         return data
       })
+      .catch(error => console.log(error))
   }
 
   getBids(offerID) {
     return this.bids.get(`/bid/${offerID}`)
       .then(({ data }) => data)
+      .catch(error => console.log(error))
   }
 
   getUserBids(userID) {
     return this.bids.get(`/bid/userBids/${userID}`)
       .then(({ data }) => data)
+      .catch(error => console.log(error))
   }
 
 
   deleteBid(bidID) {
     return this.bids.delete(`/bid/${bidID}`)
       .then(({ data }) => data)
+      .catch(error => console.log(error))
   }
 
   editBid(bid) {
@@ -38,6 +42,22 @@ class Bid {
       .then(({ data }) => data)
       .catch(error => console.log(error))
   }
+
+  acceptBid(bid) {
+    const {bidID, Status, offerID } = bid;
+    return this.bids.put(`/bid/${bidID}/accept`, { Status, offerID })
+      .then(({ data }) => data)
+      .catch(error => console.log(error))
+  }
+
+  declineBid(bid) {
+    const {bidID, Status } = bid;
+    return this.bids.put(`/bid/${bidID}/decline`, { Status })
+      .then(({ data }) => data)
+      .catch(error => console.log(error))
+  }
+
+
 
 }
 
