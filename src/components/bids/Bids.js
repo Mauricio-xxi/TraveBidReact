6227129e90c788327efbdc3db245278c02a56fdd
Margal <mgalloperalta@gmail.com>
2019-05-09 17:39:56 +0200

    Add carousel to bids

--- src/components/bids/Bids.js
+++ src/components/bids/Bids.js
@@ -1,7 +1,28 @@
 import React, { Component } from "react";
 import { withAuth } from "../../lib/AuthProvider";
 import bid from '../../lib/bid-service';
+import { Carousel } from 'react-responsive-carousel';
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+
+const BidCarouselItem = styled.div`
+  width: 70%;
+  height: 100%;
+  color: black;
+  background: ;
+  border-style: solid;
+  border-radius: 15px;
+  border-color: grey;
+  border-width: 1px;
+  padding-top: 5%;
+  padding-bottom: 5%;
+  background-image: linear-gradient(222A68);
+`;
+
+  const BidInfo = styled.p`
+    color: black;
+  `;
 
 
 class Bids extends Component {
@@ -27,19 +48,47 @@ class Bids extends Component {
   
 
   render() {
+    const { bids } = this.state;
     return (
+      // <div>
+      //   {this.state.bids.map((bid) => {
+      //     return(
+      //       <div key={bid._id}>
+      //         <Link to={`/Offer/${bid.offerID}`}>
+      //         <p>{bid.value}</p> 
+      //         <p>Budget: {bid.description}</p>
+      //         </Link>
+      //       </div>
+      //     )
+      //   })}
+      //   <p>------------------------</p>
+      // </div>
       <div>
-        {this.state.bids.map((bid) => {
-          return(
-            <div key={bid._id}>
-              <Link to={`/Offer/${bid.offerID}`}>
-              <p>{bid.value}</p> 
-              <p>Budget: {bid.description}</p>
-              </Link>
-            </div>
-          )
-        })}
-        <p>------------------------</p>
+        <h5>Your open bids</h5>
+        <Carousel 
+        showThumbs={false} 
+        showArrows={false} 
+        swipeable={true} 
+        emulateTouch={true} 
+        centerMode={true} 
+        centerSlidePercentage={80}
+        showStatus={false}
+        infiniteLoop={true}
+        showIndicators={false}
+        >
+  
+          {bids.map((bid)=>{
+            console.log(bid)
+            return (
+              <BidCarouselItem key={bid._id}>
+              <Link to={`/Offer/${bid._id}`}>
+                  <h5>{bid.location}</h5> 
+                  <h5>${bid.value}</h5>
+               </Link>
+            </BidCarouselItem>
+            )
+          })}
+        </Carousel>
       </div>
     );
   }
