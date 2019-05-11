a16d719bd8be6804da842228196b605762ce835f
Margal <mgalloperalta@gmail.com>
2019-05-11 17:34:18 +0200

    Replace carousel with css slider

--- src/components/offers/Offercarousel2.js
+++ src/components/offers/Offercarousel2.js
@@ -9,22 +9,39 @@ import { Button } from 'reactstrap';
 import '../../stylesheets/styles.css'
 import '../../stylesheets/carousel.css'
 
+
+const OfferSilderWrapper = styled.div`
+  overflow-x: scroll;
+  overflow-y: hidden;
+  white-space: nowrap;
+  margin-top: 10%;
+  margin-bottom: 5%;
+  margin-left: 5%;
+  padding-bottom: 10%;
+`;
+
+const OfferItemContainer = styled.div`
+  display: inline-block;
+  width: 60%;
+  padding:0;
+  margin-right: 10%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  border-radius: 15px;
+`;
+
 const OfferItemImage = styled.div`
   background-image: url("/city.jpg");
   background-repeat: no-repeat;
   background-position: center;
   object-fit: cover;
   background-size: cover; 
-  width: 80%;
+  width: 100%;
   height: 100%;
   color: white;
   border-width: 1px;
+  padding:0;
   padding-top: 10%;
   padding-bottom: 10%;
-`;
-
-const OfferItemContainer = styled.div`
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   border-radius: 15px;
 `;
 
@@ -32,13 +49,15 @@ const OfferItemContainer = styled.div`
 const OfferMainInfo = styled.p`
   color: white;
   font-size: 20px;
+  padding-left: 10%;
 `;
 
 const OfferInfo = styled.div`
-width: 80%;
-height: 100%;
-padding-top: 4%;
-padding-bottom: 4%;
+  width: 80%;
+  height: 100%;
+  padding-top: 4%;
+  padding-bottom: 4%;
+  padding-left: 10%;
 `;
 
 
@@ -51,20 +70,20 @@ class Offercarousel2 extends Component {
     render() {
       const { showEditOfferForm, deleteOffer, offers } = this.props;
         return (
-            <Carousel 
-            showThumbs={false} 
-            showArrows={false} 
-            swipeable={true} 
-            emulateTouch={true} 
-            centerMode={true} 
-            centerSlidePercentage={80}
-            showStatus={false}
-            infiniteLoop={true}
-            showIndicators={false}
+            // <Carousel 
+            // showThumbs={false} 
+            // showArrows={false} 
+            // swipeable={true} 
+            // emulateTouch={true} 
+            // centerMode={true} 
+            // centerSlidePercentage={80}
+            // showStatus={false}
+            // infiniteLoop={true}
+            // showIndicators={false}
             
 
-            >
-
+            // >
+              <OfferSilderWrapper>
               {offers.map((offer)=>{
                 const from = transformDate(offer.from)
                 const until = transformDate(offer.until)
@@ -85,8 +104,9 @@ class Offercarousel2 extends Component {
                  </OfferItemContainer>
                 )
               })}
+            </OfferSilderWrapper>
               
-            </Carousel>
+            // {/* </Carousel> */}
         );
     }
 };
