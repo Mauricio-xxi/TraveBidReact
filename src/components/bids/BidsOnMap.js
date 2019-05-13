0f5937bb239b125f8ff746dd3179ad8960c3b374
Margal <mgalloperalta@gmail.com>
2019-05-13 21:36:19 +0200

    Fix spacing

--- src/components/bids/BidsOnMap.js
+++ src/components/bids/BidsOnMap.js
@@ -61,40 +61,40 @@ class BidsOnMap extends Component {
     return (
       <div>
         {bids.length !== 0 ?  
-        <ReactMapGL
-        {...this.state.viewport}
-        
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-        onViewportChange={this.handleViewportChange}
-      >
-        {bids.map(bid => (
-          <Marker
-            key={bid._id}
-            latitude={bid.roomID.location.coordinates[0]}
-            longitude={bid.roomID.location.coordinates[1]}
-          >
-            <button onClick={ e =>  this.selectBid(e, bid)}>
-              <img src="/location.svg" alt=""/>
-            </button>
-          </Marker>
-        ))}
+          <ReactMapGL
+            {...this.state.viewport}
 
-        { selectedBid !== null ? (
-          <Popup
-            latitude={selectedBid.roomID.location.coordinates[0]}
-            longitude={selectedBid.roomID.location.coordinates[1]}
-            closeButton={true} closeOnClick={true}
-            onClose={this.unSelectBid }
+            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+            onViewportChange={this.handleViewportChange}
           >
-            <div>
-              <h2>{selectedBid.description}</h2>
-              <h2>{selectedBid.value}</h2>
-            </div>
-          </Popup>
-        ) : null}
+            {bids.map(bid => (
+              <Marker
+                key={bid._id}
+                latitude={bid.roomID.location.coordinates[0]}
+                longitude={bid.roomID.location.coordinates[1]}
+              >
+                <button onClick={ e =>  this.selectBid(e, bid)}>
+                  <img src="/location.svg" alt=""/>
+                </button>
+              </Marker>
+            ))}
 
-      </ReactMapGL> : <div></div> }
+            { selectedBid !== null ? (
+              <Popup
+                latitude={selectedBid.roomID.location.coordinates[0]}
+                longitude={selectedBid.roomID.location.coordinates[1]}
+                closeButton={true} closeOnClick={true}
+                onClose={this.unSelectBid }
+              >
+                <div>
+                  <h2>{selectedBid.description}</h2>
+                  <h2>{selectedBid.value}</h2>
+                </div>
+               </Popup>
+             ) : null}
+           </ReactMapGL> 
+      : <div></div> }
             
       </div>
     )
