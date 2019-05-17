a9be63b5cdca67f9220a4525024ce1efd0d2f283
Álvaro Camacho <mauricio-xxi@hotmail.com>
2019-05-17 21:59:14 +0200

    print comodities and put spinner in data

---
+++ src/components/user/Comodities.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { ReactComponent as TV } from "../../../src/assets/television.svg";
+import { ReactComponent as Wifi } from "../../../src/assets/wifi.svg";
+import { ReactComponent as Air } from "../../../src/assets/acondicionador-de-aire.svg";
+import { ReactComponent as Garage } from "../../../src/assets/garaje.svg";
+import { ReactComponent as HotWater } from "../../../src/assets/termometro.svg";
+import { ReactComponent as Washer } from "../../../src/assets/lavadora.svg";
+import { ReactComponent as Pool } from "../../../src/assets/piscina.svg";
+import { ReactComponent as PrivateBathroom } from "../../../src/assets/ducha.svg";
+import { ReactComponent as Wheelchair } from "../../../src/assets/silla-de-ruedas.svg";
+import { ReactComponent as Smoke } from "../../../src/assets/cigarrete.svg";
+import { ReactComponent as Pet } from "../../../src/assets/mascotas.svg";
+import { ReactComponent as Couples } from "../../../src/assets/pareja.svg";
+import styled from 'styled-components';
+
+const FacilitiesStyle = styled.div`
+display:flex;
+flex-wrap:wrap;
+justify-content:space-around;
+`
+
+export default class Comodities extends Component {
+  
+
+  render() {
+    const { tv, air, garage, pet, pool, privateBathroom, smoke, termo, wheelchair, wifi, washer, couples } = this.props.icons
+    console.log(this.props.icons)
+    return (
+      <FacilitiesStyle>
+        {tv === "True" ? <TV/>:<></>}
+        {wifi === "True" ? <Wifi/>:<></>}
+        {air === "True" ? <Air/>:<></>}
+        {garage === "True" ? <Garage/>:<></>}
+        {termo === "True" ? <HotWater/>:<></>}
+        {washer === "True" ? <Washer/>:<></>}
+        {pool === "True" ? <Pool/>:<></>}
+        {privateBathroom === "True" ? <PrivateBathroom/>:<></>}
+        {wheelchair === "True" ? <Wheelchair/>:<></>}
+        {smoke === "True" ? <Smoke/>:<></>}
+        {pet === "True" ? <Pet/>:<></>}
+        {couples === "True" ? <Couples/>:<></>}
+      </FacilitiesStyle>
+    )
+  }
+}
--- src/components/user/RoomCard.js
+++ src/components/user/RoomCard.js
@@ -2,18 +2,7 @@ import React, { Component } from 'react'
 import styled from 'styled-components';
 import ReactMapGL, { Marker } from "react-map-gl";
 import { Spinner } from 'reactstrap';
-import { ReactComponent as TV } from "../../../src/assets/television.svg";
-import { ReactComponent as Wifi } from "../../../src/assets/wifi.svg";
-import { ReactComponent as Air } from "../../../src/assets/acondicionador-de-aire.svg";
-import { ReactComponent as Garage } from "../../../src/assets/garaje.svg";
-import { ReactComponent as HotWater } from "../../../src/assets/termometro.svg";
-import { ReactComponent as Washer } from "../../../src/assets/lavadora.svg";
-import { ReactComponent as Pool } from "../../../src/assets/piscina.svg";
-import { ReactComponent as PrivateBathroom } from "../../../src/assets/ducha.svg";
-import { ReactComponent as Wheelchair } from "../../../src/assets/silla-de-ruedas.svg";
-import { ReactComponent as Smoke } from "../../../src/assets/cigarrete.svg";
-import { ReactComponent as Pet } from "../../../src/assets/mascotas.svg";
-import { ReactComponent as Couples } from "../../../src/assets/pareja.svg";
+import Comodities from "./Comodities";
 
 const RoomCardStyle = styled.div`
   width:100%;
@@ -72,27 +61,6 @@ export default class RoomCard extends Component {
       ...geocoderDefaultOverrides
     })
   }
-  showIcons = (facilities)=>{
-    // for (const key in facilities) {
-    //   console.log(`facilities.${key} => ${facilities[key]}`);
-
-    // }
-    if(!facilities){
-     return( <Spinner color="primary" />)
-      } else 
-        {
-          // {facilities.tv &&  <TV/>}
-          // {facilities.wifi ? <Wifi/>:""}
-          // {facilities.termo ? <HotWater/>:""}
-          // {facilities.garage ? <Garage/>:""}
-          // {facilities.pet ? <Pet/>:""}
-          // {facilities.privateBathroom ? <PrivateBathroom/>:""}
-          // {facilities.pool ? <Pool/>:""}
-          // {facilities.smoke ? <Smoke/>:""}
-          // {facilities.couples ? <Couples/>:""}
-          // {facilities.wheelchair ? <Wheelchair/>:""}
-    }
-  }
 
   render() {
     const {roomImage, roomId, description, longitude, latitude, facilities} = this.props
@@ -100,11 +68,11 @@ export default class RoomCard extends Component {
       <div> 
         <RoomCardStyle>
           <div>
-            <ImageStyle src={roomImage} alt={roomId}></ImageStyle>
+          {roomImage? <ImageStyle src={roomImage} alt={roomId}/>:<Spinner/> } 
           </div>
           <DescriptionArea>
-            <h3>{description}</h3>
-              {this.showIcons(facilities)}
+          {description? <h3>{description}</h3>:<Spinner/> } 
+              {facilities? <Comodities icons={facilities}/>:<Spinner/> }
           </DescriptionArea>
           <ReactMapGL
             ref={this.mapRef}
