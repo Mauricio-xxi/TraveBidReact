0d48e0ab14dcc0ca4068d941beec4f84cdab8fb4
Marco <mgalloperalta@gmail.com>
2019-06-05 22:20:50 +0200

    Add order by Price function comparing offers

--- src/pages/Private.js
+++ src/pages/Private.js
@@ -38,13 +38,40 @@ class Private extends Component {
       .catch( error => console.log(error) )
   }
 
+  orderOffersByPrice = async () => {
+    const { offers } = this.state;
+    const compare = (a , b ) => {
+      const priceA = a.budget;
+      const priceB = b.budget;
+      let comparison = 0;
+      if (priceA > priceB) {
+        comparison = 1;
+      } else if (priceA < priceB) {
+        comparison = -1;
+      }
+      return comparison;
+    }
+
+    const orderedOffers = [...offers].sort(compare);
+    this.setState({
+      offers: orderedOffers
+    })
+  }
+
+  orderOffersByDate = () => {
+    
+  }
+
   render() {
     const { showSearchResults, offers } = this.state;
     console.log(this.props.location)
     return (
     <div>
-      <Navbar handleShowSearchResults={this.handleShowSearchResults} location={this.props.location.pathname} />
-      { showSearchResults ? <OfferSearchResults offers={offers}/> : <div></div> }
+      <Navbar 
+        handleShowSearchResults={this.handleShowSearchResults} 
+        location={this.props.location.pathname} 
+        />
+      { showSearchResults ? <OfferSearchResults offers={offers} orderOffersByPrice = {this.orderOffersByPrice}/> : <div></div> }
       <Offers />
       <Bids/>
     </div>
